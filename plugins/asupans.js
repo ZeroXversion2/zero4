ebe4b68a24802a6515c462905ac7ae7fb233668d
zeroz <109177751+zerohay@users.noreply.github.com>
2022-08-06 23:12:28 +0700

    Update asupans.js

--- plugins/asupans.js
+++ plugins/asupans.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-	conn.sendFile(m.chat, 'https://api.zacros.my.id/asupan/random', 'asupan.mp4', 'nih asupannya kak jangan lupa subscribe channel https://m.youtube.com/channel/UC0rPHvwr0sJtaccF8Mm1Xng', m)
+	conn.sendFile(m.chat, 'https://api.zacros.my.id/asupan/random', 'asupan.mp4', 'nih asupannya kak jangan lupa subscribe channel https://www.youtube.com/channel/UCJ-AJ4DAfOL75-jpJi3m2ow', m)
 }
 handler.help = ['asupan']
 handler.tags = ['asupan']
