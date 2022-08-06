5f24355e2644dbbaa7db94643e55850578731988
zeroz <109177751+zerohay@users.noreply.github.com>
2022-08-06 23:35:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6285828357727'],
-  ['6285828357727', 'ᴢᴇʀᴏ', true]
+  ['6283178502766'],
+  ['6283178502766', 'ᴢᴇʀᴏ', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -59,7 +59,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = 'Sticker By Bot Whatsapp'
+global.wm = 'Bot On By ZEƦO'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
